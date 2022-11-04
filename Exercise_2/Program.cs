d39d282e88b234ca39b09a7d89ffd63abfb200ca
Adnurhytia <adindanurhayati30@gmail.com>
2022-11-04 16:05:45 +0700

    Memasukkan fungsi input untuk menginputkan ukuran array

--- Exercise_2/Program.cs
+++ Exercise_2/Program.cs
@@ -9,5 +9,22 @@ namespace SearchAlgorithm
 
         //variable ukuran array 
         private int n;
+
+        //get the number of elements to storein in the array
+        int i;
+
+        public void input()
+        {
+            while(true)
+            {
+                Console.WriteLine(" Masukkan Jumlah Element : ");
+                string s =Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 1380)
+                    break;
+                else
+                    Console.WriteLine("\n Array maksimum 1380 data. \n ");
+            }
+        }
     }
 }
