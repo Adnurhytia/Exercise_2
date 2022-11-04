f14a0b88ba920734acdfb301e14b6b69dc6d62c8
Adnurhytia <adindanurhayati30@gmail.com>
2022-11-04 16:44:43 +0700

    menambahkan method method sort

--- Exercise_2/Program.cs
+++ Exercise_2/Program.cs
@@ -11,20 +11,115 @@ namespace SearchAlgorithm
         private int n;
 
         //get the number of elements to storein in the array
-        int i;
+        int j;
 
         public void input()
         {
-            while(true)
+            while (true)
             {
                 Console.WriteLine(" Masukkan Jumlah Element : ");
-                string s =Console.ReadLine();
+                string s = Console.ReadLine();
                 n = Int32.Parse(s);
                 if (n <= 1380)
                     break;
                 else
                     Console.WriteLine("\n Array maksimum 1380 data. \n ");
             }
+            Console.WriteLine("");
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Masukkan element Array");
+            Console.WriteLine("----------------------");
         }
+        public void mergesort(int[] arr, int l, int m, int r)
+        {
+            // Find sizes of two
+            // subarrays to be merged
+            int n1 = m - l + 1;
+            int n2 = r - m;
+
+            // Create temp arrays
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            int i, j;
+
+            // Copy data to temp arrays
+            for (i = 0; i < n1; ++i)
+                L[i] = arr[l + i];
+            for (j = 0; j < n2; ++j)
+                R[j] = arr[m + 1 + j];
+
+            // Merge the temp arrays
+
+            // Initial indexes of first
+            // and second subarrays
+            i = 0;
+            j = 0;
+
+            // Initial index of merged
+            // subarray array
+            int k = l;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+
+            // Copy remaining elements
+            // of L[] if any
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+
+            // Copy remaining elements
+            // of R[] if any
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+        // merge()
+        void sort(int[] arr, int l, int r)
+        {
+            if (l < r)
+            {
+                // Find the middle
+                // point
+                int m = l + (r - l) / 2;
+
+                // Sort first and
+                // second halves
+                sort(arr, l, m);
+                sort(arr, m + 1, r);
+
+                // Merge the sorted halves
+                mergesort(arr, l, m, r);
+            }
+        }
+
+        // A utility function to
+        // print array of size n */
+        static void printArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
+        }
+
+ 
     }
 }
